50d2acbe7fa47bf365b252491c3de94c8784f157
Moses Chawawa <moseschawawa@gmail.com>
2022-03-01 16:35:09 +0200

    overided the queryset to include the calculated property

--- potlako_follow/models/worklist.py
+++ potlako_follow/models/worklist.py
@@ -1,10 +1,24 @@
+import imp
 from django.db import models
 from edc_search.model_mixins import SearchSlugManager
 from .worklist_model_mixin import WorkListModelMixin, BaseWorkManager
-
+from django.apps import apps as django_apps
+from queryable_properties.properties import queryable_property
+from queryable_properties.managers import QueryablePropertiesManager
+from potlako_subject.models import BaselineClinicalSummary
+from django.db.models import Subquery, QuerySet, OuterRef, Count
 
 class WorklistManager(BaseWorkManager, SearchSlugManager):
-    pass
+    def get_queryset(self):
+
+        base_clinical_summary = BaselineClinicalSummary.objects.filter(
+                subject_identifier=OuterRef('subject_identifier'))
+
+        return super().get_queryset().annotate(
+            cancer_probability=Subquery(
+                base_clinical_summary.values('cancer_probability')[:1]
+            )
+        )
 
 
 class WorkList(WorkListModelMixin):
