088f9af34e5883e88c689532efa5a3e77a8814c5
Moses Chawawa <moseschawawa@gmail.com>
2022-03-02 10:38:31 +0200

    added computed attributes to the queryset

--- potlako_follow/models/navigation_worklist.py
+++ potlako_follow/models/navigation_worklist.py
@@ -1,9 +1,35 @@
 from edc_search.model_mixins import SearchSlugManager
 from .worklist_model_mixin import WorkListModelMixin, BaseWorkManager
+from django.db.models import OuterRef, Subquery
+from potlako_subject.models import SubjectScreening, SubjectConsent, ClinicianCallEnrollment
 
 
 class NavigationWorklistManager(BaseWorkManager, SearchSlugManager):
-    pass
+    def get_queryset(self):
+        """
+        - Naviagation Work list have no screening identifier
+        - Screening Identifiers are calculated on the fly
+        - Clinical call enrollment, contain the screening identifier, and subject
+        identifier is blank, so the calculated screening identifier was used instead
+        """
+
+        screening_identifier = SubjectConsent.objects.filter(
+            subject_identifier=OuterRef('subject_identifier'))
+
+        village_town = ClinicianCallEnrollment.objects.filter(
+            screening_identifier=OuterRef('screening_identifier')
+        )
+
+        
+
+        return super().get_queryset().annotate(
+            screening_identifier=Subquery(
+                screening_identifier.values('screening_identifier')[:1]
+            ),
+            village_town = Subquery(
+                village_town.values('village_town')[:1]
+            )
+        )
 
 
 class NavigationWorkList(WorkListModelMixin):
